c1c28b8a77a463153bade60ddd7ddacc106551db
btd716 <btd716@gmail.com>
2020-10-25 21:13:51 -0400

    Update VendingMachineContents.java

--- VendingMachine/src/main/java/com/sg/vendingmachine/dto/VendingMachineContents.java
+++ VendingMachine/src/main/java/com/sg/vendingmachine/dto/VendingMachineContents.java
@@ -4,16 +4,36 @@
  */
 package com.sg.vendingmachine.dto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class VendingMachineContents {
+    
+    String name;
+    float cost;
+    int count;
+    
+    
+    
+    /*
+    Map <String, Integer> vmContents = new HashMap<>();
+        vmContents.put("Coke", 0.75);
+        vmContents.put("Sprite", 0.75);
+        vmContents.put("Dr Pepper", 0.75);
+    */
+    
+    
+    
     
     // what constructor do I write???
     
-    
+    /*
     // the variables of items in the vending machine
     private String a1 = "coke",
                 a2 = "sprite",
                 a3 = "dr pepper";
     
+    // price of items
     public double p1 = .75,
                 p2 = .75,
                 p3 = .75;
@@ -22,7 +42,7 @@ public class VendingMachineContents {
     public int countCoke = 10,
                 countSprite = 15,
                 countDrPepper = 7;
-
+    */
    
     
     
